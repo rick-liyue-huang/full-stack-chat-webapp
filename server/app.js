18be3630a92516e2494e6da88b8dbc826502fa5a
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-01-10 16:01:15 +1100

    add client and test the connection between client and server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const db = require('./models');
+const postRouter = require('./routes/Posts');
 const PORT = 3500;
+
+// solve the problem of cross-origin
+app.use(cors());
+// let the response know json format
+app.use(express.json());
+// import routes
+app.use('/posts', postRouter);
+
 
 db.sequelize.sync().then(() => {
 	app.listen(PORT, () => {
